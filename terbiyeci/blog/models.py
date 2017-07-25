eaee7f608ace02da14db98b1420f11c234fb475a
Umut Karcı <cediddi@google.com>
2017-07-25 19:54:11 +0300

    modeller eklendi

--- terbiyeci/blog/models.py
+++ terbiyeci/blog/models.py
@@ -1,3 +1,17 @@
+import datetime
 from django.db import models
 
-# Create your models here.
+
+class ShortNews(models.Model):
+    title = models.CharField(max_length=200)
+    source = models.URLField()
+    created = models.DateTimeField(default=datetime.datetime.now)
+    featured_for = models.DateTimeField(default=None, blank=True, null=True)
+    image = models.ImageField(blank=True, null=True)
+    score = models.SmallIntegerField()
+    report_count = models.PositiveSmallIntegerField()
+    categories = models.ManyToManyField("Category")
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
